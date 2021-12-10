e99895af11efa2cc28e4bcb7330152d5fbc858fc
Minh212 <minhhqgch190485@fpt.edu.vn>
2021-12-10 20:51:31 +0700

    ok

--- controllers/trainer.js
+++ controllers/trainer.js
@@ -8,21 +8,5 @@ router.get('/',requiresLogin,(req,res)=>{
     res.render('trainerIndex', { userInfo: user })
 })
 
-router.get('/addUser',(req,res)=>{
-    res.render('addUser')
-})
-//Submit add User
-router.post('/addUser',(req,res)=>{
-    const name = req.body.txtName
-    const role = req.body.Role
-    const pass= req.body.txtPassword
-    const objectToInsert = {
-        userName: name,
-        role:role,
-        password: pass
-    }
-    insertObject("Users",objectToInsert)
-    res.render('adminIndex')
-})
 
 module.exports = router;
