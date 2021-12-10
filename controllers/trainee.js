fcad430ea16e8dc8d85bac3a9891792fc832b8d5
hphoa261200 <87641798+hphoa2612@users.noreply.github.com>
2021-12-10 22:50:06 +0700

    oke

--- controllers/trainee.js
+++ controllers/trainee.js
@@ -8,6 +8,16 @@ router.get('/',(req,res)=>{
     res.render('traineeIndex')
 })
 
+router.get('/viewClass', (req,res)=>{
+    res.render("viewClassmates")
+})
 
+router.get('/viewMyCourse', (req,res)=>{
+    res.render("viewCourse")
+})
+
+router.get('/update', (req,res)=>{
+    res.render('updateProfile')
+})
 
 module.exports = router;
