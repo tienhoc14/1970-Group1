5754ae5177223dda00632b4d0d04235fcd314930
Minh212 <minhhqgch190485@fpt.edu.vn>
2021-12-14 22:35:19 +0700

    update trainer

--- controllers/trainer.js
+++ controllers/trainer.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { insertObject, getDB } = require('../databaseHandler')
+const { insertObject, getDB, ObjectId } = require('../databaseHandler')
 const { requireTrainer } = require('../projectLibrary')
 const router = express.Router()
 
@@ -22,6 +22,39 @@ router.get('/profileTrainer', requireTrainer, async(req, res) => {
     const dbo = await getDB()
     const trainer = await dbo.collection("Trainers").findOne({ "userName": user.name })
 
+    res.render('profileTrainer', { data: trainer })
+})
+
+
+router.get('/updateProfileTrainer', requireTrainer, async(req, res) => {
+    const user = req.session["Trainer"]
+    const dbo = await getDB()
+    const trainer = await dbo.collection("Trainers").findOne({ "userName": user.name })
+    res.render('updateProfileTrainer', { data: trainer })
+})
+
+router.post('/updateProfileTrainer', requireTrainer, async(req, res) => {
+    const id = req.body.txtId
+    const name = req.body.trainerName
+    const age = req.body.trainerAge
+    const phone = req.body.phone
+    const spec = req.body.spec
+    const address = req.body.address
+
+    const updateToTrainers = {
+        $set: {
+            name: name,
+            age: age,
+            speciality: spec,
+            address: address,
+            phone_number: phone
+        }
+    }
+    const filter = { _id: ObjectId(id) }
+    const dbo = await getDB()
+    await dbo.collection("Trainers").updateOne(filter, updateToTrainers)
+
+    const trainer = await dbo.collection("Trainers").findOne({ "_id": ObjectId(id) })
     res.render('profileTrainer', { data: trainer })
 })
 
