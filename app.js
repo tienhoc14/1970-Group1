f37b8b1955e714581163fbefaee5c3c88f759860
tienhoc14 <87641821+tienhoc14@users.noreply.github.com>
2021-12-21 15:11:54 +0700

    update

--- app.js
+++ app.js
@@ -12,9 +12,8 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: '124447yd@@$%%#', cookie: { maxAge: 900000 }, saveUninitialized: false, resave: false }))
 
-app.get('/', requiresLogin, (req, res) => {
-    const user = req.session["User"]
-    res.render('index', { userInfo: user })
+app.get('/', (req, res) => {
+    res.render('login')
 })
 
 app.post('/login', async(req, res) => {
